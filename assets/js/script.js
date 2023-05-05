7fef2801ac9fab2505977edc2eefbea93037c2df
Redsskull <ivry91@gmail.com>
2023-05-05 14:31:58 +0200

    add basic declartions and function to script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("hello");
+/**
+ * Decalring constants for the DOM elements
+ * and possible chouices
+ */
+
+const buttons = document.getElementsByClassName("planets");
+const score = document.getElementById("score-area");
+const choices = ["planet1", "planey2", "planet3"];
+
+
+/**
+ * Add event listener to the buttons
+ */
+
+for (let button of buttons) {
+
+    button.addEventListener("click", function ()  {
+        let playerChoice = this.getAttribute("data-choice");
+        playGame(playerChoice)
+    });
+
+}
+
+/**
+ * The game. accepts the player choice from 
+ * the data-choice
+ */
+
+function playGame(playerChoice) {
+    let computerChoice = math.floor(Math.random() * 3);
+
+    let result = checkWinner(choices[computerChoice.choice], choices[playerChoice]);
+
+    updateScore(result);
+}
